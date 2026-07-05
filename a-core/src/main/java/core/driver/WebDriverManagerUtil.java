908600e43390358604cf76f8c040a849c8e24b06
thandeka-nkosi <thandeka.dev@outlook.com>
2026-07-05 22:56:16 +0200

    Add headless Chrome options for cloud execution

--- a-core/src/main/java/core/driver/WebDriverManagerUtil.java
+++ a-core/src/main/java/core/driver/WebDriverManagerUtil.java
@@ -55,8 +55,15 @@ public class WebDriverManagerUtil {
                         case "chrome":
                             WebDriverManager.chromedriver().setup();
                             ChromeOptions chromeOptions = new ChromeOptions();
+                            chromeOptions.addArguments("--headless=new");
+                            chromeOptions.addArguments("--no-sandbox");
+                            chromeOptions.addArguments("--disable-dev-shm-usage");
+                            chromeOptions.addArguments("--disable-gpu");
+                            chromeOptions.addArguments("--window-size=1920,1080");
+                            chromeOptions.addArguments("--remote-allow-origins=*");
                             createdDriver = new ChromeDriver(chromeOptions);
                             break;
+
                         case "edge":
                             WebDriverManager.edgedriver().setup();
                             EdgeOptions edgeOptions = new EdgeOptions();
